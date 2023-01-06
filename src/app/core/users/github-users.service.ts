ced284c5fdf809c1082b346aec776a06d54e2ab6
Luca Del Puppo <luca@delpuppo.net>
2023-01-06 19:50:49 +0100

    add zod

--- src/app/core/users/github-users.service.ts
+++ src/app/core/users/github-users.service.ts
@@ -1,20 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { z, ZodSchema } from 'zod';
 
-interface GitHubUser {
-  login: string;
-  id: string;
-  avatar_url: string;
-  url: string;
-  repos_url: string;
-  type: 'User';
-}
+const GitHubUser = z.object({
+  login: z.string(),
+  id: z.string(),
+  avatar_url: z.string(),
+  url: z.string(),
+  repos_url: z.string(),
+  type: z.union([z.literal('User'), z.literal('Organization')]),
+});
 
-interface GitHubUserSearchResponse {
-  items: GitHubUser[];
-  total_count: number;
-}
+type GitHubUser = z.infer<typeof GitHubUser>;
+
+const GitHubUserSearchResponse = z.object({
+  items: z.array(GitHubUser),
+  total_count: z.number(),
+});
+
+type GitHubUserSearchResponse = z.infer<typeof GitHubUserSearchResponse>;
+
+const zodValidation =
+  <T extends ZodSchema>(schema: T) =>
+  (source: Observable<unknown>): Observable<z.infer<T>> =>
+    source.pipe(map((obj) => schema.parse(obj)));
 
 @Injectable({ providedIn: 'root' })
 export class GitHubUsersService {
@@ -22,9 +32,10 @@ export class GitHubUsersService {
 
   searchUsers(query: string): Observable<GitHubUser[]> {
     return this.httpClient
-      .get<GitHubUserSearchResponse>(
-        `https://api.github.com/search/users?q=${query}`
-      )
-      .pipe(map((response) => response.items));
+      .get(`https://api.github.com/search/users?q=${query}`)
+      .pipe(
+        zodValidation(GitHubUserSearchResponse),
+        map((response) => response.items)
+      );
   }
 }
